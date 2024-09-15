33f113b2d14a3e9423f88e509ba13348d77e1ee3
Paul <paul@tirikon.com>
2024-09-15 16:51:11 +0300

    for PR

--- dist/enums/global.js
+++ dist/enums/global.js
@@ -1,9 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DefaultClient = void 0;
+exports.UnitStatus = exports.BoardStatus = exports.DefaultClient = void 0;
 var DefaultClient;
 (function (DefaultClient) {
     DefaultClient["default"] = "akeyless";
     DefaultClient["camera_installation"] = "akeyless";
 })(DefaultClient || (exports.DefaultClient = DefaultClient = {}));
+var BoardStatus;
+(function (BoardStatus) {
+    BoardStatus[BoardStatus["ReadyForInstallation"] = 1] = "ReadyForInstallation";
+    BoardStatus[BoardStatus["Installed"] = 2] = "Installed";
+    BoardStatus[BoardStatus["Malfunction"] = 3] = "Malfunction";
+})(BoardStatus || (exports.BoardStatus = BoardStatus = {}));
+var UnitStatus;
+(function (UnitStatus) {
+    UnitStatus[UnitStatus["NewOrder"] = 0] = "NewOrder";
+    UnitStatus[UnitStatus["Installed"] = 1] = "Installed";
+    UnitStatus[UnitStatus["WaitingForService"] = 2] = "WaitingForService";
+    UnitStatus[UnitStatus["InstalledButNotActive"] = 3] = "InstalledButNotActive";
+    UnitStatus[UnitStatus["NewInstallation"] = 4] = "NewInstallation";
+})(UnitStatus || (exports.UnitStatus = UnitStatus = {}));
 //# sourceMappingURL=global.js.map
