6dae86d54972a012d957575547b8a2bc65865c6c
avrhahm sason <avi6190i@gmail.com>
2025-06-23 13:27:28 +0300

    fixes

--- src/types/socket.ts
+++ src/types/socket.ts
@@ -5,17 +5,13 @@ export interface RedisUpdatePayload<UpdateType extends RedisUpdateType, DataType
     collection_name: string;
     update_type: UpdateType;
     data: DataType;
-    firebase?: {
-        merge?: boolean;
-    };
     update_time: number;
 }
 
 export interface SocketCallbackResponse {
     success: boolean;
-    found: boolean;
-    message?: string;
     key?: string;
     data?: any;
-    saved_in?: "redis" | "firebase";
+    found?: boolean;
+    message?: string;
 }
