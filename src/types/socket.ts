836eb4c61af6ed34e2a267b1312257e8d2306406
avrhahm sason <avi6190i@gmail.com>
2025-06-24 16:37:14 +0300

    done

--- src/types/socket.ts
+++ src/types/socket.ts
@@ -1,3 +1,25 @@
+export const REDIS_UPDATES_PREFIX = "redis_updates";
+
+export const SOCKET_EVENTS = {
+    SET_DATA: "set_data",
+    GET_DATA: "get_data",
+    DELETE_DATA: "delete_data",
+    SUBSCRIBE_COLLECTIONS: "subscribe_collections",
+    UNSUBSCRIBE_COLLECTIONS: "unsubscribe_collections",
+    CLEAR_ALL_REDIS_DATA: "clear_all_redis_data",
+    DISCONNECT: "disconnect",
+} as const;
+
+export type SocketEventMap = {
+    [SOCKET_EVENTS.SET_DATA]: any;
+    [SOCKET_EVENTS.GET_DATA]: any;
+    [SOCKET_EVENTS.DELETE_DATA]: any;
+    [SOCKET_EVENTS.SUBSCRIBE_COLLECTIONS]: any;
+    [SOCKET_EVENTS.UNSUBSCRIBE_COLLECTIONS]: any;
+    [SOCKET_EVENTS.CLEAR_ALL_REDIS_DATA]: any;
+    [SOCKET_EVENTS.DISCONNECT]: any;
+};
+
 export type RedisUpdateType = "update" | "add" | "delete";
 
 export interface RedisUpdatePayload<UpdateType extends RedisUpdateType, DataType = any> {
@@ -14,4 +36,10 @@ export interface SocketCallbackResponse {
     data?: any;
     found?: boolean;
     message?: string;
+}
+
+export interface CollectionConfig {
+    key?: string;
+    sync_direction?: "firebase_to_redis" | "redis_to_firebase";
+    sync_period?: number;
 }
