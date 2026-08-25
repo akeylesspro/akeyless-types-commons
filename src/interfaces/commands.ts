5ded357c3dcbad2c5edeed7f4a7101d311c0f8b3
Avraham Sason <avi6190i@gmail.com>
2026-08-25 16:26:48 +0300

    extend DeviceCommand interface and add new types for device command handling

--- src/interfaces/commands.ts
+++ src/interfaces/commands.ts
@@ -1,4 +1,5 @@
-import { BoardMaker } from "../types";
+import { BoardMaker, TObject } from "../types";
+import { firebase_timestamp, GprsItem, TimestampType } from "./global";
 
 export type DeviceCommandOption =
     | {
@@ -10,7 +11,110 @@ export type DeviceCommandOption =
 export interface DeviceCommand {
     id: string;
     label_key: string;
-    sms: DeviceCommandOption[];
-    gprs: DeviceCommandOption[];
     maker: BoardMaker;
+    group: number;
+    sms?: DeviceCommandOption[];
+    gprs?: DeviceCommandOption[];
 }
+
+export type DeviceCommandType = "sms" | "gprs";
+
+export interface DeviceCommandDetails {
+    command_key: string;
+    command_type: "device";
+    user_name: string;
+    user_id: string;
+    command_id: string;
+}
+
+export interface SendDeviceCommandBody {
+    car_number: string;
+    command_type: DeviceCommandType;
+    command_id?: string;
+    messages?: DeviceCommandOption[];
+    label_key?: string;
+    request_id?: string;
+}
+
+export interface SendDeviceCommandResponse {
+    request_id: string;
+    recipient: string;
+    command_key: string;
+    command_type: DeviceCommandType;
+    sent: number;
+}
+
+export type DeviceCommandStatus = "done" | "in_progress" | "failed" | "responded";
+
+export type DeviceCommandProgressStatus = DeviceCommandStatus | "idle" | "sending" | "timeout";
+
+export interface DeviceCommandItem {
+    id: string;
+    content: string;
+    status: string;
+    response: string;
+}
+
+export interface SendDeviceCommandResult {
+    success: boolean;
+    request_id: string;
+    error?: string;
+}
+
+export interface SmsOutItem {
+    id: string;
+    content: string;
+    external_id: string;
+    recipient: string;
+    service: string;
+    status: string;
+    timestamp: firebase_timestamp;
+    details?: TObject<any>;
+    updated?: firebase_timestamp;
+    response?: string;
+}
+
+export interface SmsInItem {
+    id: string;
+    content: string;
+    service: string;
+    external_id: string;
+    timestamp: TimestampType;
+    sender: string;
+}
+
+export interface GprsInItem {
+    id: string;
+    content: string;
+    external_id: number;
+    recipient: string;
+    timestamp: TimestampType;
+    commandId?: string;
+}
+
+export interface CommandHistoryItemBase {
+    commandKey: string;
+    commandType: string;
+    userId: string;
+    userName: string;
+    sendingTime: TimestampType;
+    status: DeviceCommandStatus;
+    commandId: string;
+    recipient: string;
+}
+
+export interface GroupedSmsCommand extends CommandHistoryItemBase {
+    type: "SMS";
+    messages: SmsOutItem[];
+    firstSmsTime: TimestampType;
+    lastSmsTime: TimestampType;
+}
+
+export interface GroupedGprsCommand extends CommandHistoryItemBase {
+    type: "GPRS";
+    commands: GprsItem[];
+    firstCommandTime: TimestampType;
+    lastCommandTime: TimestampType;
+}
+
+export type CommandHistoryItem = GroupedSmsCommand | GroupedGprsCommand;
