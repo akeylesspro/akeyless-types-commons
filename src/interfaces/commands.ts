01bf0d4170de41a2ea36b4cdfad57a24720494d2
Avraham Sason <avi6190i@gmail.com>
2026-08-27 12:05:12 +0300

    v++

--- src/interfaces/commands.ts
+++ src/interfaces/commands.ts
@@ -38,7 +38,7 @@ export interface SendDeviceCommandBody {
 }
 
 export interface SendDeviceCommandResponse {
-    command_id: string;
+    request_id: string;
 }
 
 export type DeviceCommandStatus = "done" | "in_progress" | "failed" | "responded";
